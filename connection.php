67a62d5df116b861d9c6c2411ead29a140fff814
morgane-marechal <morgane.marechal@laplateforme.io>
2022-12-05 17:32:52 +0100

    ajout formualire inscription et connexion

---
+++ connection.php
@@ -0,0 +1,92 @@
+<?php
+session_start();
+?>
+<?php
+    include 'dbconnect.php';
+?>
+<head>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+        <meta http-equiv="x-ua-compatible" content="IE=edge" />
+        <title>Connection</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href=index.php>Home</a></li>
+                <?php if (isset($_SESSION['login'])){?>
+                <li><a href=livre-or.php>Le livre d'or</a></li>
+                <li><a href=commentaires.php><span class="material-symbols-outlined">add_comment</span></a></li>
+                <li><a href=profil.php><span class="material-symbols-outlined">manage_accounts</span></a></li>
+                <?php } ?>
+                <li><a href=inscription.php><span class="material-symbols-outlined">how_to_reg</span></a></li>
+                <li><a href=connection.php><span class="material-symbols-outlined">login</span></a></li>
+
+            </ul>
+        </nav>
+    </header>
+    <div id="sessionlog">
+        <?php if (isset($_SESSION['login'])){
+                        echo "<p>Bonjour ".$_SESSION['login'].". Vous êtes connecté</p>";
+                    }
+        ?>
+    </div>
+    <main>
+            <section id="connexion_form">
+                <form action="" method="post">
+                    <h3>Création de compte</h3>
+                    <div id="login">  
+                    <input type="text" name="login" id="login" placeholder="Login*" required minlength="5"> 
+                    </div>
+                    <input type="password" name="password" id="password" placeholder="Password*" required minlength="5">
+                    </select>
+                    <input class="submit" type="submit" value="Envoyer">
+                    <i class="small">* Champs obligatoires avec 5 caractères minimum</i>
+                    <?php
+                        $login = mysqli_real_escape_string($mysqli,htmlspecialchars($_POST['login'])); //protection pour éviter injection SQL malveillante
+                        $_SESSION['login'] = $login;
+                        $_SESSION['password'] = $password;
+                        //------------------------code pour le formulaire de connexion --------------------------
+                    
+                        if(isset($_POST['login']) && isset($_POST['password'])){
+                            if($login !== "" && $password !== ""){
+                                $password_post=$_POST['password'];
+                                $request=$mysqli->query("SELECT * FROM utilisateurs where login = '$login'");
+                                $result=$request->fetch_all();
+                                $password_hash=$result[0][2]; // <-retrouve mot de passe crypté
+                                if (password_verify($password_post, $password_hash)) {    //<=password verify pour checker un mot de passe crypté
+                                    $requete = "SELECT count(*) FROM utilisateurs where login = '$login' and password= '$password_hash'"; //recherche si login et password cripté existe
+                                    $exec_requete = mysqli_query($mysqli,$requete);
+                                    $reponse = mysqli_fetch_array($exec_requete);
+                                    $count = $reponse['count(*)'];
+                                
+                                        if($count!=0) // login et mot de passe correctes
+                                        {
+                                            echo "Bravo vous êtes connectés!";
+                                            $_SESSION['login'] = $login;
+                                            $_SESSION['password'] = $password_post;
+                                            header('Location: http://localhost/livre-or/profil.php'); // <- redirection vers la page admin
+                                            exit();
+                                        }
+                                        else
+                                        {
+                                        $error_info = "Utilisateur ou mot de passe invalide!";
+                                        
+                                        }
+
+                                    }else{
+                                        $error_info = "Utilisateur ou mot de passe invalide!";
+                                        echo $error_info;
+                                    }
+                                }
+                            }
+                                mysqli_close($mysqli);
+                        ?>
+                </form>
+            </section>
+    
+    
+        
--- inscription.php
+++ inscription.php
@@ -1,27 +1,97 @@
 <?php
-    include 'dbconnect.php';
+session_start();
 ?>
-
+<?php include 'dbconnect.php';?>
 <head>
     <meta charset="utf-8"/>
     <link rel="stylesheet" type="text/css" href="style.css" />
         <meta http-equiv="x-ua-compatible" content="IE=edge" />
-        <title>Home</title>
+        <title>Inscription</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+
 </head>
 <body>
     <header>
         <nav>
             <ul>
                 <li><a href=index.php>Home</a></li>
-                <li><a href=inscription.php>Inscription</a></li>
-                <li><a href=connection.php>Connexion</a></li>
+                <?php if (isset($_SESSION['login'])){?>
+                <li><a href=livre-or.php>Le livre d'or</a></li>
+                <li><a href=commentaires.php><span class="material-symbols-outlined">add_comment</span></a></li>
+                <li><a href=profil.php><span class="material-symbols-outlined">manage_accounts</span></a></li>
+                <?php } ?>
+                <li><a href=inscription.php><span class="material-symbols-outlined">how_to_reg</span></a></li>
+                <li><a href=connection.php><span class="material-symbols-outlined">login</span></a></li>
+
             </ul>
         </nav>
     </header>
+    <div id="sessionlog">
+        <?php if (isset($_SESSION['login'])){
+                        echo "<p>Bonjour ".$_SESSION['login'].". Vous êtes connecté</p>";
+                    }
+        ?>
+    </div>
     <main>
-        <form>
-            
-        </form>
+            <section id="connexion_form">
+                <form action="" method="post">
+                    <h3>Création de compte</h3>
+                    <div id="login">  
+                    <input type="text" name="login" id="login" placeholder="Login*" required minlength="5"> 
+                    </div>
+                    <input type="password" name="password" id="password" placeholder="Password*" required minlength="5">
+                    <input type="password" name="conf_password" id="conf_password" placeholder="Confirmation du mot de passe*" required minlength="2">
+                    </select>
+                    <input class="submit" type="submit" value="Envoyer">
+                    <i class="small">* Champs obligatoires avec 5 caractères minimum</i>
+
+        
+    <?php
+     $login = mysqli_real_escape_string($mysqli,htmlspecialchars($_POST['login'])); //protection pour éviter injection SQL malveillante
+     $password = mysqli_real_escape_string($mysqli,htmlspecialchars($_POST['password'])); 
+     $checkpassword = mysqli_real_escape_string($mysqli,htmlspecialchars($_POST['conf_password'])); //protection pour éviter injection SQL malveillante
+    
+
+
+     
+
+     $check_login = "SELECT count(*) FROM utilisateurs where login = '$login'";
+     $exec_requete = mysqli_query($mysqli,$check_login);
+     $reponse = mysqli_fetch_array($exec_requete);
+     $count = $reponse['count(*)'];
+
+    if ($count==1){
+        $error_login="<p>Ce login est déjà pris, veuillez en choisir un autre!</p>";
+        echo $error_login;
+    }else{
+        if (($password==$checkpassword) && (!empty($login)) && (!empty($password))){
+        $password = password_hash($password, PASSWORD_DEFAULT); 
+            $newuser = "INSERT INTO utilisateurs ( login, password)
+            VALUES( '$login', '$password')";
+        
+            if ($mysqli->query($newuser) === TRUE) {
+                //echo "Vous avez ajouté un utilisateur avec succés";
+                header('Location: http://localhost/livre-or/connection.php'); // <- redirection vers la page connexion si l'inscription fonctionne
+                exit();
+                } else {
+                echo "Erreur: " . $newuser . "
+                " . $mysqli->error;
+                }
+
+        }elseif ((empty($name)) OR (empty($firstname)) OR (empty($password))){
+            $error_empty="<p>L'un des champs du formulaire est vide<p>";
+            echo $error_empty;
+        }elseif ($password!=$checkpassword){
+            $error_password="<p>Veuillez confirmer correctement votre mot de passe!</p>";
+            echo $error_password;
+        }
+    }
+    $mysqli->close();
+    
+    ?>
+
+    </form>
+</section>
         
 
     </main>
