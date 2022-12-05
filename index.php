fb2888660efa76d8af1e35dfc89778a6bec09d8d
morgane-marechal <morgane.marechal@laplateforme.io>
2022-12-05 13:44:09 +0100

    ajout connection base de donnée et header sur index et inscription

---
+++ index.php
@@ -0,0 +1,20 @@
+<head>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+        <meta http-equiv="x-ua-compatible" content="IE=edge" />
+        <title>Home</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href=index.php>Home</a></li>
+                <li><a href=inscription.php>Inscription</a></li>
+                <li><a href=connection.php>Connexion</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+
+    </main>
+</body>
---
+++ inscription.php
@@ -0,0 +1,28 @@
+<?php
+    include 'dbconnect.php';
+?>
+
+<head>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+        <meta http-equiv="x-ua-compatible" content="IE=edge" />
+        <title>Home</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href=index.php>Home</a></li>
+                <li><a href=inscription.php>Inscription</a></li>
+                <li><a href=connection.php>Connexion</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <form>
+            
+        </form>
+        
+
+    </main>
+</body>
